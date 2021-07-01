92f5378f9f5044305f2ab2f70647d88c5eb54607
nekrut <nekrut12@gmail.com>
2021-07-01 06:46:38 +0300

    Домашняя работа к лекции №3 (Spring_1)

---
+++ src/main/java/ru/gb/market/MainConfig.java
@@ -0,0 +1,47 @@
+package ru.gb.market;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainConfig {
+    ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping(value = "/add_form")
+    public String getAddForm(){
+        return "add_form";
+    }
+    @PostMapping(value = "/add")
+    public String addProduct(@RequestParam int id, @RequestParam String title, @RequestParam float price){
+        Product product = new Product(id, title, price);
+        productRepository.addProduct(product);
+        return "redirect:/add_form";
+    }
+
+    @GetMapping(value = "products")
+    public String getProducts(Model model){
+        model.addAttribute("products", productRepository.getProducts());
+        return "products";
+    }
+
+    @GetMapping(value = "find_product")
+    public String getProductById(Model model, @RequestParam int id){
+        Product product = productRepository.getProductById(id);
+        model.addAttribute("products", product);
+        return "products";
+    }
+
+    @GetMapping(value = "index")
+    public String getMainPage(){
+        return "index";
+    }
+}
